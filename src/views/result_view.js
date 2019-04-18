ece435fec283e3d6ee207625948a34689f0425b2
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-18 22:11:41 +0100

    Extension complete

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -8,23 +8,37 @@ class ResultView {
 
   bindEvents() {
     PubSub.subscribe('SelectedInstrument:ready', (event) => {
-      console.log('SelectedInstrument:ready working');
-      this.populate_result(event.detail);
+      this.clearResult();
+      this.populateHeader(event.detail);
+      this.populateResult(event.detail);
       this.populateUL(event.detail);
     });
   };
 
-  populate_result(detail) {
+  clearResult() {
+    this.element.textContent = "";
+  };
+
+  populateHeader(detail) {
+    const resultHead = document.createElement('h1');
+    resultHead.textContent = detail.name;
+    this.element.appendChild(resultHead);
+  };
+
+  populateResult(detail) {
     const resultText = document.createElement('p');
     resultText.textContent = detail.description;
     this.element.appendChild(resultText);
   };
 
-  populateUL(data) {
-    
-    data.instruments.forEach((instrument) => {
-      this.element.appendChild();
+  populateUL(detail) {
+    const resultUL = document.createElement('ul');
+    detail.instruments.forEach((instrument) => {
+      const resultLI = document.createElement('li');
+      resultLI.textContent = instrument;
+      resultUL.appendChild(resultLI);
     });
+    this.element.appendChild(resultUL);
   };
 
 };
